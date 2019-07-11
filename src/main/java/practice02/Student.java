da26c4668a2f90aed392c9ae2c72f9b8e016a551
SamsonLin <153011490@qq.com>
2019-07-11 21:13:34 +0800

    finsh practice02 Person class and Student class and pass it test

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,19 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int kclass;
+
+    public Student(String name, int age, int kclass) {
+        super(name, age);
+        this.kclass = kclass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class "+this.kclass+".";
+    }
+
+    public int getKlass() {
+        return kclass;
+    }
 }
