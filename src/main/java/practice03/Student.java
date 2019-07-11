735214dcfcf3ccca791d2af0be0d895bb4e9518f
lrh <153011490@qq.com>
2019-07-12 00:25:30 +0800

    finsh practice02 and pass it test

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,21 @@
 package practice03;
 
-public class Student {
+package practice02;
+
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name, int age, int kclass) {
+        super(name, age);
+        this.kclass = kclass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class "+this.kclass+".";
+    }
+
+    public int getKlass() {
+        return kclass;
+    }
 }
